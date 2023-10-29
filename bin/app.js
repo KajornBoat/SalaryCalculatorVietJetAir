b8f019dbca57048925df760cac7a3cae3f2b4e29
Kajorn Boonjaroen <47472718+KajornBoat@users.noreply.github.com>
2023-10-30 01:44:01 +0700

    Update Route in View

--- bin/app.js
+++ bin/app.js
@@ -1,18 +1,15 @@
-
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var app = express();
-var port = 80
+var Router = require('./routes/index');
 
-var Router = require('./src/routes/index');
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // app.use(logger('dev'));
@@ -23,14 +20,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/",Router);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(port, () => console.log(`Server is running on PORT ${port}`))
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
